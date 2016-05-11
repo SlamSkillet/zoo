e4dcb80dd17e3c307a1272c11e5b7a58894a4a31
SlamSkillet <scs2177@columbia.edu>
2016-05-11 19:15:52 -0400

    imported svg and selected

--- js/zoodraw.js
+++ js/zoodraw.js
@@ -1,4 +1,14 @@
- var start = Date.now();
-        project.importSVG(document.getElementById('svg'));
-    console.log(Date.now() - start);
-    // console.log(project.exportJSON());
+var start = Date.now();
+
+var path = project.importSVG(document.getElementById('svg'));
+
+path.fullySelected = true;
+
+function onFrame(event) {
+
+	// path.strokeWidth += 0.1;
+	//path.segments[0].point.y += 0.1;
+
+}
+console.log(Date.now() - start);
+console.log(project.exportJSON());
