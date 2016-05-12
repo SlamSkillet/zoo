71808ac88e0972a131739ed3cb6738161d8ad458
SlamSkillet <scs2177@columbia.edu>
2016-05-12 09:41:18 -0400

    drew new zoo plan and got gates to move

--- js/zoodraw.js
+++ js/zoodraw.js
@@ -1,29 +1,47 @@
-/* var pathData = "M52,117.75h23v-68h32v34h51v-53h36v75h51v35h-77v-31h-37v75H92v-36H46v-30.999";
+// var pathData = "M52,117.75h23v-68h32v34h51v-53h36v75h51v35h-77v-31h-37v75H92v-36H46v-30.999";
 
-var path = new Path(pathData);
-path.strokeColor = 'white';
-path.strokeWidth = 2; 
-path.fullySelected = true; */
+// var path = new Path(pathData);
+// path.strokeColor = 'white';
+// path.strokeWidth = 2; 
+// path.fullySelected = true; 
 
 
-var path2 = project.importSVG(document.getElementById('svg2'));
+var path2 = project.importSVG(document.getElementById('svg'));
 // console.log(project.activeLayer.children)
-console.log(path2.children);
+console.log(path2.children[40]);
+console.log(path2.children[45]);
+console.log(path2.children[49]);
 
-/* function onFrame(event) {
-	path.segments[0].point.y += 0.1;
-} */
+// // gate 1
+// path2.children[40].fullySelected = true;
+
+// // gate 2
+// path2.children[45].fullySelected = true;
+
+// // gate 3
+// path2.children[49].fullySelected = true;
 
 function onFrame(event) {
+	path2.children[40].segments[0].point.x += 0.1;
+	path2.children[40].segments[0].point.y += 0.1;
+
+	path2.children[45].segments[0].point.x += 0.1;
+	path2.children[45].segments[0].point.y += 0.1;
+
+	path2.children[49].segments[0].point.x += 0.1;
+	path2.children[49].segments[0].point.y += 0.1;
+} 
+
+// function onFrame(event) {
 	
-	// path
-	path2.children[1].segments[5].point.y += 0.1;
-	path2.children[1].segments[5].point.x += 0.1;
+// 	// path
+// 	path2.children[1].segments[5].point.y += 0.1;
+// 	path2.children[1].segments[5].point.x += 0.1;
 
-	// circle
-	path2.children[2].matrix.ty += 0.1;
-	path2.children[2].matrix.tx += 0.1;
+// 	// circle
+// 	path2.children[2].matrix.ty += 0.1;
+// 	path2.children[2].matrix.tx += 0.1;
 
-}
+// }
 
 // console.log(project.exportJSON());
