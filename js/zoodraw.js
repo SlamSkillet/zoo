66a1883423cc69ff2cdaae481b14f3d6a652c43e
SlamSkillet <scs2177@columbia.edu>
2016-05-11 20:25:41 -0400

    better manipulation of svg

--- js/zoodraw.js
+++ js/zoodraw.js
@@ -1,19 +1,29 @@
-var start = Date.now();
-
-var pathData = "M52,117.75h23v-68h32v34h51v-53h36v75h51v35h-77v-31h-37v75H92v-36H46v-30.999";
+/* var pathData = "M52,117.75h23v-68h32v34h51v-53h36v75h51v35h-77v-31h-37v75H92v-36H46v-30.999";
 
 var path = new Path(pathData);
 path.strokeColor = 'white';
-path.strokeWidth = 2;
+path.strokeWidth = 2; 
+path.fullySelected = true; */
 
-/* var path = project.importSVG(document.getElementById('svg'));
-path.fullySelected = true;
-console.log(path.strokeWidth);
-console.log(path.segments); */
+
+var path2 = project.importSVG(document.getElementById('svg2'));
+// console.log(project.activeLayer.children)
+console.log(path2.children);
+
+/* function onFrame(event) {
+	path.segments[0].point.y += 0.1;
+} */
 
 function onFrame(event) {
-	path.segments[0].point.y += 0.1;
+	
+	// path
+	path2.children[1].segments[5].point.y += 0.1;
+	path2.children[1].segments[5].point.x += 0.1;
+
+	// circle
+	path2.children[2].matrix.ty += 0.1;
+	path2.children[2].matrix.tx += 0.1;
+
 }
 
-console.log(Date.now() - start);
-console.log(project.exportJSON());
+// console.log(project.exportJSON());
